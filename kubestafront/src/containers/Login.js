4034d598aa570e09614ebe09166761fb96cd6240
pandorasNox <itspandorasnox@gmail.com>
2018-07-29 09:31:58 +0200

    add login form

--- kubestafront/src/containers/Login.js
+++ kubestafront/src/containers/Login.js
@@ -3,14 +3,73 @@ import { withSiteData } from 'react-static'
 
 import styled from 'styled-components'
 
-export default withSiteData(() => (
+import { Formik } from 'formik';
+
+export default withSiteData((props) => (
   <Grid>
     <Headline>Login</Headline>
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+      }}
+      onSubmit={(
+        values,
+        { setSubmitting, setErrors /* setValues and other goodies */ }
+      ) => {
+        const { email, password } = values;
+        setTimeout(() => {
+          setSubmitting(false);
+          props.history.push('/')
+        }, 2000)
+        console.log(email, password);
+      }}
+      render={({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+      }) => (
+        <StyledForm onSubmit={handleSubmit}>
+          <div>
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+          </div>
+          <div>
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </div>
+        </StyledForm>
+      )}
+    />
   </Grid>
 ))
 
-const Grid = styled.section`
+const Grid = styled.div`
   display: grid;
+  justify-content: center;
+`
+
+const StyledForm = styled((props) => <form {...props} />)`
+  width: 100%;
 `
 
 const Headline = styled.h1`
